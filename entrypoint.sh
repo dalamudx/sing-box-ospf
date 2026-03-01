75a1ed2068c0346d2c945f533aa0660687a261fc
admin <n386345222@hotmail.com>
2026-03-01 13:48:01 +0800

    feat: add preprocessing for BIRD routing files and optimize scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 /usr/bin/envsubst < /app/bird/bird.template > /app/bird/bird.conf
 /usr/bin/crontab /app/cron
+if [ ! -f /app/bird/routers4.conf ] || [ ! -f /app/bird/routers6.conf ]
+then
+    touch /app/bird/routes4.conf
+    touch /app/bird/routes6.conf
+fi
 /usr/bin/supervisord -c /etc/supervisord.conf
